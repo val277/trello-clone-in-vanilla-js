5887093a889c3ffb71a2c26161740ce4e871ee42
Valentin <218587221+val277@users.noreply.github.com>
2025-08-21 18:06:19 +0200

    added vite to build

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  base: "./",
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        app: resolve(__dirname, "src/app.html"),
+      },
+    },
+  },
+});
